84df7eac475dbcbf395d19d843de488b7258dc42
정현수 <wjdgustn15@kakao.com>
2022-01-18 13:13:23 +0900

    Feat : Update BE-초기세팅(README.md,pom.xml,WebCurationApplication.java)

--- backend-java/src/main/java/com/web/curation/WebCurationApplication.java
+++ backend-java/src/main/java/com/web/curation/WebCurationApplication.java
@@ -1,9 +1,14 @@
 package com.web.curation;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+//TODO 나중에 디비 추가하면 DataSourceAutoConfiguration.class을 지워주세요. 보안 문제가 해결 되면,  SecurityAutoConfiguration.class을 지우세요.
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class WebCurationApplication {
 
 	public static void main(String[] args) {

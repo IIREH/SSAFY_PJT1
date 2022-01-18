b86b514a3a0c555e09463d7f2c77591b120f015b
정현수 <wjdgustn15@kakao.com>
2022-01-18 13:15:18 +0900

    Feat : Update BE 초기세팅(IndexController.java,Exception,ApiUtils.java)

---
+++ backend-java/src/main/java/com/web/curation/api/IndexController.java
@@ -0,0 +1,29 @@
+package com.web.curation.api;
+
+import com.web.curation.model.BasicResponse;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
+        @ApiResponse(code = 403, message = "Forbidden", response = BasicResponse.class),
+        @ApiResponse(code = 404, message = "Not Found", response = BasicResponse.class),
+        @ApiResponse(code = 500, message = "Failure", response = BasicResponse.class) })
+
+@CrossOrigin(origins = { "http://localhost:3000" })
+@RestController
+@Slf4j
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        log.info("call index");
+        return "index";
+    }
+}

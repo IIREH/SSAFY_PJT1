d67cd99522c32b649b6958294fda3710e312d835
양승우 <aquageek94@naver.com>
2022-01-25 01:38:00 +0900

    Feat : NavBar & Log-in & Register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,28 @@ import "./App.css";
 import Row from "./Row";
 import Banner from "./Banner";
 import "./Banner.css";
-import Nav from "./Nav";
+//import Nav from "./Nav";
+import { Route, Routes } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+import PostPage from "./pages/PostPage";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       {/* Inserting design components and fetch param from tmdb API */}
-      <Nav />
+      <Routes>
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/@:username">
+          <Route index element={<PostListPage />} />
+          <Route path=":postId" element={<PostPage />} />
+        </Route>
+      </Routes>
       <Banner />
       <Row title="추천 예술 작품" />
       <Row title="사진전" />
@@ -17,10 +32,9 @@ function App() {
       <Row title="클래식" />
       <Row title="사진" />
       <Row title="오페라" />
-
     </div>
   );
-}
+};
 
 // Exporting App function. Making it available
 export default App;

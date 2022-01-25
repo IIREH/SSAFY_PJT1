339aed790192e4927d100daf5b1ca0d95fe83249
양승우 <aquageek94@naver.com>
2022-01-25 10:55:53 +0900

    Feat : NavBar & Log-in & Register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,27 @@ import Row from "./Row";
 import Banner from "./Banner";
 import "./Banner.css";
 import Nav from "./Nav";
+import { Route, Routes } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
     <div className="app">
       {/* Inserting design components and fetch param from tmdb API */}
-      <Nav />
+      <Routes>
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/@:username">
+          <Route index element={<PostListPage />} />
+          <Route path=":postId" element={<PostPage />} />
+        </Route>
+      </Routes>
       <Banner />
       <Row title="추천 예술 작품" />
       <Row title="사진전" />
@@ -17,7 +32,6 @@ function App() {
       <Row title="클래식" />
       <Row title="사진" />
       <Row title="오페라" />
-
     </div>
   );
 }

3852d8876f49e677b3a1c22df9988508b97bfc76
양승우 <aquageek94@naver.com>
2022-01-27 00:43:59 +0900

    Add : Sample for presentation

--- frontend/src/Banner.js
+++ frontend/src/Banner.js
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
 
 function Banner() {
-  
   // Setting up useState
 
   //   useEffect (short term memory)
   useEffect(() => {
-    async function fetchData() {
-    }
+    async function fetchData() {}
     // Pulling data from API
     fetchData();
   }, []);
@@ -20,20 +18,22 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("")`,
+        backgroundImage: `url("https://www.kopis.or.kr/upload/pfmPoster/PF_PF185931_220120_104206.jpg")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">
-          {}
-        </h1>
+        <h1 className="banner__title">{}</h1>
         <div className="banner__buttons">
           <button className="banner__button">글 보러가기</button>
           <button className="banner__button">쪼아요</button>
         </div>
 
         <h1 className="banner__description">
+        <br></br><br></br><br></br><br></br><br></br>
+          슈배르트가 작곡한 세계 최고의 띵곡...!<br></br>
+          포브스 선정 꼭 들어야 되는 음악 1위...!<br></br>
+          양승우 선정 심심할때 들으면 좋은 음악 2위...!!
         </h1>
       </div>
       {/* Empty fade div with bottom fade effect  */}

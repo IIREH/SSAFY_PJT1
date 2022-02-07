30d17d18d7464ba4d51b59199e76dd6638dd0b8a
양승우 <aquageek94@naver.com>
2022-02-07 12:53:27 +0000

    Deleted frontend/public/favicon.ico, frontend/public/index.html, frontend/public/manifest.json, frontend/server/src/api/auth/auth.ctrl.js, frontend/server/src/api/auth/index.js, frontend/server/src/api/posts/index.js, frontend/server/src/api/posts/posts.ctrl.js, frontend/server/src/api/index.js, frontend/server/src/lib/checkLoggedIn.js, frontend/server/src/lib/jwtMiddleware.js, frontend/server/src/models/post.js, frontend/server/src/models/user.js, frontend/server/src/createFakeData.js, frontend/server/src/index.js, frontend/server/src/main.js, frontend/server/.env, frontend/server/.eslintrc.json, frontend/server/.gitignore, frontend/server/.prettierrc, frontend/server/jsconfig.json, frontend/server/package-lock.json, frontend/server/package.json, frontend/server/yarn.lock, frontend/src/components/auth/AuthForm.js, frontend/src/components/auth/AuthTemplate.js, frontend/src/components/auth/UpdateUserInfoForm.js, frontend/src/components/common/Button.js, frontend/src/components/common/Header.js, frontend/src/components/common/Responsive.js, frontend/src/components/user/FollowComponent.js, frontend/src/components/user/PostsComponent.js, frontend/src/containers/auth/LoginForm.js, frontend/src/containers/auth/RegisterForm.js, frontend/src/containers/auth/UpdateUserInfoFormContainer.js, frontend/src/containers/common/HeaderContainer.js, frontend/src/containers/user/ProfileContainer.js, frontend/src/img/ag-logo.png, frontend/src/img/img.jpg, frontend/src/lib/api/auth.js, frontend/src/lib/api/client.js, frontend/src/lib/styles/palette.js, frontend/src/lib/createRequestSaga.js, frontend/src/modules/auth.js, frontend/src/modules/index.js, frontend/src/modules/loading.js, frontend/src/modules/profile.js, frontend/src/modules/user.js, frontend/src/pages/auth/LoginPage.js, frontend/src/pages/auth/RegisterPage.js, frontend/src/pages/auth/UpdateUserInfoPage.js, frontend/src/pages/post/PostListPage.js, frontend/src/pages/post/PostPage.js, frontend/src/pages/post/WritePage.js, frontend/src/pages/user/ProfilePage.js, frontend/src/pages/Main.js, frontend/src/pages/NotFoundPage.js, frontend/src/App.css, frontend/src/App.js, frontend/src/App.test.js, frontend/src/Banner.css, frontend/src/Banner.js, frontend/src/Nav.css, frontend/src/Nav.js, frontend/src/Row.css, frontend/src/Row.js, frontend/src/axios.js, frontend/src/config.java, frontend/src/index.css, frontend/src/index.js, frontend/src/logo.svg, frontend/src/reportWebVitals.js, frontend/src/requests.js, frontend/src/serviceWorker.js, frontend/src/setupTests.js, frontend/.gitignore, frontend/README.md, frontend/package-lock.json, frontend/package.json, frontend/yarn.lock files

--- frontend/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "",
-});
-
-// Exporting instance object.Making it available
-export default instance;
--- frontend/src/requests.js
+++
@@ -1,8 +0,0 @@
-// API KEY
-const API_KEY = "2a0a294a9f6e0594afb7d514a796f71c";
-
-
-const request = { };
-
-// Exporting request object. Making it available
-export default request;

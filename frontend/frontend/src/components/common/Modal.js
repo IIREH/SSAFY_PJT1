47ff7ae31958f39d92e89a551ba8eaccf4cc775b
YunyLee <happydbsdud@gmail.com>
2022-02-10 13:54:43 +0900

    Change : Change ModalCss to BootstrapModal

--- frontend/frontend/src/components/common/Modal.js
+++ frontend/frontend/src/components/common/Modal.js
@@ -1,37 +1,37 @@
-import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+// import { useEffect, useRef } from "react";
+// import { Link } from "react-router-dom";
 
-const Modal = ({ handleClose, open, onDelete }) => {
-  const ref = useRef();
+// const Modal = ({ handleClose, open, onDelete }) => {
+//   const ref = useRef();
   
-  useEffect(() => {
-    const handleOutsideClick = (e) => {
-      if (open && ref.current && !ref.current.contains(e.target)) {
-        handleClose();
-      }
-    };
+//   useEffect(() => {
+//     const handleOutsideClick = (e) => {
+//       if (open && ref.current && !ref.current.contains(e.target)) {
+//         handleClose();
+//       }
+//     };
 
-    document.addEventListener("click", handleOutsideClick);
+//     document.addEventListener("click", handleOutsideClick);
 
-    return () => {
-      // Cleanup the event listener
-      document.removeEventListener("click", handleOutsideClick);
-    };
-  }, [open, handleClose]);
-  return (
-    <div className={`${open ? "modal-container" : "close"}`}>
-      <div ref={ref} className="modal">
-        <h2>프로필 상세화면 모달창 입니다 ! 쿠키모양을 누르면 모달창이 떠요 !</h2>
-        <button className="button" onClick={handleClose}>
-          Close
-        </button>
-        <button>
-          <Link to="/updateUserInfo">회원정보수정</Link>
-        </button>
-        <button className="button" onClick={onDelete}>회원탈퇴</button>
-      </div>
-    </div>
-  );
-};
+//     return () => {
+//       // Cleanup the event listener
+//       document.removeEventListener("click", handleOutsideClick);
+//     };
+//   }, [open, handleClose]);
+//   return (
+//     <div className={`${open ? "modal-container-custom" : "close-custom"}`}>
+//       <div ref={ref} className="modal-custom">
+//         <h2>프로필 상세화면 모달창 입니다! </h2>
+//         <button className="button" onClick={handleClose}>
+//           Close
+//         </button>
+//         <button>
+//           <Link to="/updateUserInfo">회원정보수정</Link>
+//         </button>
+//         <button className="button" onClick={onDelete}>회원탈퇴</button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Modal;
+// export default Modal;
